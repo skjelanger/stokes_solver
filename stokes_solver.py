856ba3f0bc48934afd62994da01aa3e7e2244775
Kristoffer Skjelanger <skje@hvl.no>
2025-05-15 10:17:23 +0200

    Starting to non-dimensionalize

--- stokes_solver.py
+++ stokes_solver.py
@@ -920,7 +920,7 @@ if __name__ == "__main__":
     
     # Define constants
     geometry_length = 0.001 # meters 0.001 is 1mm
-    mesh_size = geometry_length * 0.0007 # meters
+    mesh_size = geometry_length * 0.005 # meters
     inner_radius = geometry_length * 0.47
     geometry_height = 0.000091 # 91 micrometer thickness
     mu = 0.00089 # Viscosity Pa*s
